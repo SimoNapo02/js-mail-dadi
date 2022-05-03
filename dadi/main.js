82f134a4186614c0a771034eb132b7357d86d27e
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-05-03 18:56:56 +0200

    js dadi pc

--- dadi/main.js
+++ dadi/main.js
@@ -1,5 +1,7 @@
 // Generare un numero random da 1 a 6 per l'utente
 const random = Math.floor(Math.random() * 6)+1
+console.log(random)
 // Generare un numero random da 1 a 6 per il computer
-
+const randompc = Math.floor(Math.random() * 6)+1
+console.log(randompc)
 // stabilire il vincitore tramite numero più alto
