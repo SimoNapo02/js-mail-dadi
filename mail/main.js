5bac9dbb7ef24ff5de5c83f3a92218bb26f20edf
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-05-03 14:51:45 +0200

    first commit

---
+++ mail/main.js
@@ -0,0 +1,6 @@
+// Chiedi all'utente la sua email
+let user = prompt("Please enter your e-mail", "SimNap@virgilio.it");
+// Controllare che sia nella lista di accesso
+
+// 1.se è sulla, stampa messaggio accesso
+// 2.se non è sulla lista, stampa messaggio di negazione
