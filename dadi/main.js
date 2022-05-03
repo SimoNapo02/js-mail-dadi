4f222427c9dbf76735e2b48bbeace20e13ee8b96
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-05-03 19:13:10 +0200

    js dadi

--- dadi/main.js
+++ dadi/main.js
@@ -5,3 +5,14 @@ console.log(random)
 const randompc = Math.floor(Math.random() * 6)+1
 console.log(randompc)
 // stabilire il vincitore tramite numero più alto
+    // se user>pc allora vince,
+    //  altrimenti perde se minore, 
+    // altrimenti pareggio se uguale
+
+if(random>randompc){
+    console.log("user ha vinto!")
+} else if(random === randompc){
+    console.log("pareggio!")
+} else {
+    console.log("pc ha vinto!")
+}
