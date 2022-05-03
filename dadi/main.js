e45e76d30089cb9f9f856152828734af17953024
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-05-03 17:44:23 +0200

    aggiunto comandi console js email

---
+++ dadi/main.js
@@ -0,0 +1,5 @@
+// Generare un numero random da 1 a 6 per l'utente
+
+// Generare un numero random da 1 a 6 per il computer
+
+// stabilire il vincitore tramite numero più alto
--- mail/main.js
+++ mail/main.js
@@ -1,6 +1,20 @@
 // Chiedi all'utente la sua email
-let user = prompt("Please enter your e-mail", "SimNap@virgilio.it");
+const user = prompt("inserisci la tua email");
 // Controllare che sia nella lista di accesso
+const email = ["email1@gmail.com", "email2@gmail.com", "email3@gmail.com", "email5@gmail.com", "email6@gmail.com", ]
+let result = false;
 
-// 1.se è sulla, stampa messaggio accesso
+for(let i = 0;i < email.length; i++){
+ // 1.se è sulla, stampa messaggio accesso
 // 2.se non è sulla lista, stampa messaggio di negazione
+    if(user === email[i]){
+        result = true
+    }
+}
+
+if(result){
+    console.log("accesso garantito")}
+else{
+        console.log("accesso negato");
+    };
+
